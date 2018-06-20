d2cb4cc885f4d98ef69930d5c471b2d8d5451d2f
kukujiabo <kukujiabo@163.com>
2018-06-20 11:41:32 +0800

    123

--- src/app/Api/Partner.php
+++ src/app/Api/Partner.php
@@ -24,6 +24,18 @@ class Partner extends BaseApi {
         'order' => 'order|string|false||排序',
         'fields' => 'fields|string|false||字段'
       
+      ],
+
+      'accept' => [
+      
+        'member_id' => 'member_id|int|true||会员id', 
+
+      ],
+
+      'reject' => [
+      
+        'member_id' => 'member_id|int|false||会员id',
+      
       ]
     
     ]);
@@ -39,6 +51,30 @@ class Partner extends BaseApi {
   public function getApplyList() {
   
     return $this->dm->getApplyList($this->retriveRuleParams(__FUNCTION__));
+  
+  }
+
+  /**
+   * 同意合伙人
+   * @desc 同意合伙人
+   *
+   * @return int num
+   */
+  public function accept() {
+  
+    return $this->dm->accept($this->retriveRuleParams(__FUNCTION__));
+  
+  }
+
+  /**
+   * 拒绝申请
+   * @desc 拒绝申请
+   *
+   * @return int num
+   */
+  public function reject() {
+  
+    return $this->dm->reject($this->retriveRuleParams(__FUNCTION__));
   
   }
 
--- src/app/Domain/PartnerDm.php
+++ src/app/Domain/PartnerDm.php
@@ -3,9 +3,29 @@ namespace App\Domain;
 
 class PartnerDm {
 
-  public function getApplyList() {
+  public function getApplyList($data) {
   
     return \App\request('App.AgentApply.GetList', $data);
+  
+  }
+
+  public function accept($data) {
+
+    $data['state'] = 1;
+
+    $data['confirm_at'] = date('Y-m-d H:i:s');
+
+    return \App\request('App.AgentApply.Edit', $data);
+  
+  }
+
+  public function reject($data) {
+  
+    $data['state'] = -1;
+
+    $data['reject_at'] = date('Y-m-d H:i:s');
+
+    return \App\request('App.AgentApply.Edit', $data);
   
   }
 
