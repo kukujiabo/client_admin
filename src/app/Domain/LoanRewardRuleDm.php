0d7d58f26506861e2dae3cc3e3e19e6f1c51f8b9
kukujiabo <kukujiabo@163.com>
2018-07-01 18:51:08 +0800

    123

--- src/app/Domain/LoanRewardRuleDm.php
+++ src/app/Domain/LoanRewardRuleDm.php
@@ -4,6 +4,8 @@ namespace App\Domain;
 class LoanRewardRuleDm {
 
   public function create($data) {
+
+    $params['member_id'] = 0;
   
     return \App\request('App.LoanRewardRule.Create', $params);
   
