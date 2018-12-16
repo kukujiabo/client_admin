f32aa31e325934cbd0a87fa0d31ac59836cc7e1f
meroc <398515393@qq.com>
2018-12-16 23:16:44 +0800

    123

--- src/app/Api/ChannelCode.php
+++ src/app/Api/ChannelCode.php
@@ -23,7 +23,7 @@ class ChannelCode extends BaseApi {
       'getList' => [
       
         'channel_id' => 'channel_id|int|true||渠道id',
-        'bank_id' => 'bank_id|int|true||银行id',
+        'bank_id' => 'bank_id|int|false||银行id',
         'page' => 'page|int|false|1|页码',
         'page_size' => 'page_size|int|false|20|每页条数'
       
